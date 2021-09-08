bae932309c93e23fa4b607d809b7d79dabf5fb40
Nikita Kravchuk <nikita.kravchuk@ukr.net>
2021-09-08 17:53:03 +0300

    Homework 2

---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import bgImg from '../img/bg1.jpeg'
+import s from './layout.module.css'
+
+const Layout = ({
+    title,
+    desc,
+    urlBg = '',
+    colorBg
+}) => {
+    return(
+        <section className={s.root} style={urlBg ? {backgroundImage: `url(${bgImg})`} : {backgroundColor: colorBg}}>
+            <div className={s.wrapper}>
+                <article>
+                    <div className={s.title}>
+                        <h3>{title}</h3>
+                        <span className={s.separator}></span>
+                    </div>
+                    <div className={`{${s.desc}, ${s.full}}`}>
+                        <p>{desc}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    )
+}
+
+export default Layout;
